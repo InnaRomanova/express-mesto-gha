3a3f0e0d8cb8d0187d735fe3e37b0350ee884ac2
unknown <romanovainnap@yandex.ru>
2022-10-16 21:14:13 +0300

    fix getUser server code

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const SUCCESS_CODE = 200;
 const ERROR_CODE = 400;
 const NOT_FOUND_CODE = 404;
-// const SERVER_CODE = 500;
+const SERVER_CODE = 500;
 
 module.exports.getUsers = (req, res) => {
   User.find({}).then((users) => res.status(SUCCESS_CODE).send({ users }));
@@ -11,8 +11,20 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUser = (req, res) => {
   User.findOne({ _id: req.params.id })
-    .then((user) => res.status(SUCCESS_CODE).send({ user }))
-    .catch(() => res.status(NOT_FOUND_CODE).send({ message: 'Пользователь с таким id не найден' }));
+    .then((user) => {
+      if (!user) {
+        res.status(NOT_FOUND_CODE).send({ message: 'Пользователь с таким id не найден' });
+        return;
+      }
+      res.status(SUCCESS_CODE).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE).send(err);
+        return;
+      }
+      res.status(SERVER_CODE).send(err);
+    });
 };
 
 // eslint-disable-next-line consistent-return
