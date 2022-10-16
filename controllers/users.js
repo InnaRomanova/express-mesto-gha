f8a1af8af338102340fabbca8893adedb181c535
unknown <romanovainnap@yandex.ru>
2022-10-16 20:59:51 +0300

    fix getUser

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 const SUCCESS_CODE = 200;
 const ERROR_CODE = 400;
-const NOT_FOUND_CODE = 404;
+// const NOT_FOUND_CODE = 404;
 // const SERVER_CODE = 500;
 
 module.exports.getUsers = (req, res) => {
@@ -12,7 +12,7 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUser = (req, res) => {
   User.findOne({ _id: req.params.id })
     .then((user) => res.status(SUCCESS_CODE).send({ user }))
-    .catch(() => res.status(NOT_FOUND_CODE).send({ error: 'Пользователь с таким id не найден' }));
+    .catch(() => res.status(ERROR_CODE).send({ message: 'Пользователь с таким id не найден' }));
 };
 
 // eslint-disable-next-line consistent-return
