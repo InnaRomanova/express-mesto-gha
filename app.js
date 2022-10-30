2c57f49c28aec479ea5bf8e7e580870415604adb
unknown <romanovainnap@yandex.ru>
2022-10-30 10:20:43 +0300

    авторизация

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const routers = require('./routes/index');
+const errorsHandler = require('./middlewares/errorsHandler');
 
 const { PORT = 3000 } = process.env; // Слушаем 3000 порт
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routers);
 app.use(errors());
+app.use(errorsHandler);
 
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 
